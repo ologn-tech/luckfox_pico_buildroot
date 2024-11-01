0cc998e8d637256a7ff31bb2e58e1a6fa2f248d2
Huy Duong <huy.duong@ologn.tech>
2024-11-02 00:34:21 +0700

    Add luckfox-pico package

---
+++ package/luckfox-pico/luckfox-pico.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# luckfox-pico
+#
+################################################################################
+
+LUCKFOX_PICO_SITE = https://github.com/LuckfoxTECH/luckfox-pico.git
+LUCKFOX_PICO_SITE_METHOD = git
+LUCKFOX_PICO_VERSION = e79fd219757ebe534a31a4c566138b053af8ce3b
+
+$(eval $(generic-package))
