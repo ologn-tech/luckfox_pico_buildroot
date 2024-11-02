9bfcce27fa5f60cc7d8caa810d132cdbdbcac64c
Huy Duong <huy.duong@ologn.tech>
2024-11-02 18:59:23 +0700

    Add new scripts for USB gadget support

--- board/luckfox-pico/post-build.sh
+++ board/luckfox-pico/post-build.sh
@@ -2,6 +2,5 @@
 
 BOARD_DIR=$(dirname "$0")
 
-cp -f "$BOARD_DIR/S50usbdevice" "$TARGET_DIR/etc/init.d/"
-cp -f "$BOARD_DIR/S99loadmodules" "$TARGET_DIR/etc/init.d/"
-cp -f "$BOARD_DIR/S99usb0config" "$TARGET_DIR/etc/init.d/"
+install -m 0755 $BOARD_DIR/S20loadmodules $TARGET_DIR/etc/init.d/
+install -m 0755 $BOARD_DIR/S49usbgadget $TARGET_DIR/etc/init.d/
